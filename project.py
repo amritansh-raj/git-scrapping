dadfce3b39ae35b40da3a9dfbffc0919ed715203
amritansh-raj <amritansh.raj21@gmail.com>
2023-10-04 19:41:15 +0530

    repo done

--- project.py
+++ project.py
@@ -22,13 +22,24 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 if response.status_code == 200:
     data = json.loads(response.text)
-    users = data['payload']['results']
-    # print(users)
-
-    for user in users:
-        name = user.get('name', '')
-        followers = user.get('followers', 0)
-        repo = user.get('repos', 0)
-        print(f"Name: {name}\nFollowers: {followers}\nRepos: {repo}\n")
+    
+    if searchfor == 'users':
+        results = data['payload']['results']
+        for user in results:
+            name = user.get('name', '')
+            followers = user.get('followers', 0)
+            repo = user.get('repos', 0)
+            bio = user.get('profile_bio', '')
+            print(f"Name: {name}\nFollowers: {followers}\nRepos: {repo}\nBio: {bio}\n\n")
+    
+    elif searchfor == 'repositories':
+        results = data['payload']['results']
+        for repo in results:
+            name = repo['repo']['repository']['name']
+            owner = repo['repo']['repository']['owner_login']
+            stars = repo.get('followers', 0)
+            forks = repo.get('forks', 0)
+            description = repo.get('hl_trunc_description', '')
+            print(f"Name: {name}\nOwner: {owner}\nStars: {stars}\nForks: {forks}\nDescription: {description}\n\n")
 else:
     print("Failed to retrieve the page. Status code:", response.status_code)
